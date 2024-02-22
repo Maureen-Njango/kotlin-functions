62248f2134008a2a8a407a62ad0c7a5481a0f40b
Maueen-Njango <maureennjangombira@gmail.com>
2024-02-22 07:53:27 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main(){
+    printName("Njango")
+    modulus(56,67)
+    add(8,9,10,11)
+    facts("I love eating")
+}
+fun printName(name: String) {
+    println("Hello $name")
+}
+    fun modulus(num1: Int, num2: Int){
+        var result= num1%num2
+        println(result)
+    }
+    fun add(num1: Int ,num2: Int ,num3: Int ,num4: Int){
+        var sum=num1+num2+num3+num4
+        println(sum)
+    }
+
+    fun facts(text:String){
+        var result=text
+        println(result)
+    }
+
+
+
+
+
+
+
